b711e18e62bc5db588012fc15832956075a2834b
Saurabh Kumar <kumarsaurabhsingh05@gmail.com>
2023-12-15 14:47:52 +0530

    Added a profile view path + Thymeleaf

--- src/main/java/com/saurabh/resumeportal/HomeController.java
+++ src/main/java/com/saurabh/resumeportal/HomeController.java
@@ -1,28 +1,91 @@
 package com.saurabh.resumeportal;
 
+import com.saurabh.resumeportal.models.Education;
+import com.saurabh.resumeportal.models.Job;
+import com.saurabh.resumeportal.models.UserProfile;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
+import java.util.Optional;
 
 @Controller
 public class HomeController {
 
+    @Autowired
+    UserProfileRepository userProfileRepository;
+
     @GetMapping("/")
     public String home() {
-        return "Hello";
+        return "index";
     }
 
     @GetMapping("/edit")
-    public String editPage() {
-        return "edit page";
+    public String edit(Model model, Principal principal, @RequestParam(required = false) String add) {
+        String userId = principal.getName();
+        model.addAttribute("userId", userId);
+        Optional<UserProfile> userProfileOptional = userProfileRepository.findByUserName(userId);
+        userProfileOptional.orElseThrow(() -> new RuntimeException("Not found: " + userId));
+        UserProfile userProfile = userProfileOptional.get();
+        if ("job".equals(add)) {
+            userProfile.getJobs().add(new Job());
+        } else if ("education".equals(add)) {
+            userProfile.getEducations().add(new Education());
+        } else if ("skill".equals(add)) {
+            userProfile.getSkills().add("");
+        }
+
+        model.addAttribute("userProfile", userProfile);
+        return "profile-edit";
+    }
+
+    @GetMapping("/delete")
+    public String delete(Model model, Principal principal, @RequestParam String type, @RequestParam int index) {
+        String userId = principal.getName();
+        Optional<UserProfile> userProfileOptional = userProfileRepository.findByUserName(userId);
+        userProfileOptional.orElseThrow(() -> new RuntimeException("Not found: " + userId));
+        UserProfile userProfile = userProfileOptional.get();
+        if ("job".equals(type)) {
+            userProfile.getJobs().remove(index);
+        } else if ("education".equals(type)) {
+            userProfile.getEducations().remove(index);
+        } else if ("skill".equals(type)) {
+            userProfile.getSkills().remove(index);
+        }
+        userProfileRepository.save(userProfile);
+        return "redirect:/edit";
+    }
+
+    @PostMapping("/edit")
+    public String postEdit(Model model, Principal principal, @ModelAttribute UserProfile userProfile) {
+        String userName = principal.getName();
+        Optional<UserProfile> userProfileOptional = userProfileRepository.findByUserName(userName);
+        userProfileOptional.orElseThrow(() -> new RuntimeException("Not found: " + userName));
+        UserProfile savedUserProfile = userProfileOptional.get();
+        userProfile.setId(savedUserProfile.getId());
+        userProfile.setUserName(userName);
+        userProfileRepository.save(userProfile);
+        return "redirect:/view/" + userName;
     }
 
     @GetMapping("/view/{userId}")
-    public String view(@PathVariable("userId") String userId, Model model) {
+    public String view(Principal principal, @PathVariable String userId, Model model) {
+        if (principal != null && principal.getName() != "") {
+            boolean currentUsersProfile = principal.getName().equals(userId);
+            model.addAttribute("currentUsersProfile", currentUsersProfile);
+        }
+        String userName = principal.getName();
+        Optional<UserProfile> userProfileOptional = userProfileRepository.findByUserName(userId);
+        userProfileOptional.orElseThrow(() -> new RuntimeException("Not found: " + userId));
+
         model.addAttribute("userId", userId);
-        return "profile";
+        UserProfile userProfile = userProfileOptional.get();
+        model.addAttribute("userProfile", userProfile);
+        System.out.println(userProfile.getJobs());
+
+        return "profile-templates/" + userProfile.getTheme() + "/index";
     }
 
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,10 @@
 insert into user (id, user_name, password, active, roles) values
-(1, 'foo', 'foo', true, 'USER'),
-(2, 'bar', 'bar', true, 'USER')
-;
+(1, 'einstein', 'einstein', true, 'USER'),
+(2, 'newton', 'newton', true, 'USER'),
+(3, 'saurabh', 'kumar', true, 'USER');
+
+
+insert into user_profile (id, user_name, theme, summary, first_name, last_name, email, phone, designation) values
+(1, 'einstein', 1, 'Developed the theory of relativity, one of the two pillars of modern physics. My work is also known for its influence on the philosophy of science.', 'Albert', 'Einstein', 'einstein@gmail.com', '111-111-1111', 'Theoretical physicist'),
+(2, 'newton', 2, 'Widely recognised as one of the most influential scientists of all time and as a key figure in the scientific revolution', 'Isaac', 'Newton', 'newton@gmail.com', '222-222-2222', 'Mathematician, physicist, astronomer, theologian, and author'),
+(3, 'saurabh', 3, 'Passionate Postgraduate with MCA Degree and Aspiring Java Developer, Eager to Learn, Contribute, and Innovate', 'Saurabh', 'Kumar Singh', 'kumarsaurabhsingh477@gmail.com', '799-221-2983', 'Java Backend Developer and Aspiring author');
